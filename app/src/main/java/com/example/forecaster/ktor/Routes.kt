c770f6a6990bb7e2c2b91b685cdf7587e9ca3ef3
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2022-05-10 13:01:23 +0700

    moved interceptor

--- app/src/main/java/com/example/forecaster/ktor/Routes.kt
+++
@@ -1,6 +0,0 @@
-package com.example.forecaster.ktor
-
-object Routes {
-    private const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
-    const val WEATHER_URL = "$BASE_URL/forecast?appid=b7eedb84f15757c5da8941a00663136d&units=metric&lang=ru"
-}
