2e5fd804fcf3b5d38a8b2f833fb1022fb60c5ea3
Winnie-the-Pooh2019 <gregsonjackk@gmail.com>
2022-05-13 15:33:33 +0700

    Data loading in ViewModel changed

---
+++ app/src/main/java/com/example/forecaster/MainViewModel.kt
@@ -0,0 +1,41 @@
+package com.example.forecaster
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import com.example.forecaster.data.MainRepository
+import com.example.forecaster.data.model.Weather
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlin.coroutines.CoroutineContext
+
+class MainModel(private val city: String, private val repository: MainRepository, activity: AppCompatActivity) :
+    AndroidViewModel(activity.application) {
+
+    val weatherList: MutableLiveData<MutableList<Weather>> by lazy {
+        val liveData = MutableLiveData<MutableList<Weather>>()
+        CoroutineScope(Dispatchers.Default).launch {
+            val data = repository.getWeatherFromNet(city)
+
+            if (data.list.isEmpty()) {
+                Snackbar.make(
+                    activity.findViewById(R.id.main_layout),
+                    R.string.connection_failed,
+                    Snackbar.LENGTH_SHORT
+                ).show()
+
+                repository.getWeatherFromDb(city).list.toMutableList()
+            } else
+                data.list.toMutableList().apply {
+                    sortWith { left, right -> left.date.compareTo(right.date) }
+                }
+
+            liveData.postValue(data.list.toMutableList())
+        }
+
+        liveData
+    }
+}
